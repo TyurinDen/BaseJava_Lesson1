c34aa9e08d5fe132c3de1c2f9449918584d4ffbc
Den <tyurinden@ya.ru>
2018-01-10 15:06:31 +0400

    added some code

--- src/com/urise/webapp/model/ArrayStorage.java
+++ src/com/urise/webapp/model/ArrayStorage.java
@@ -5,27 +5,53 @@ import java.util.Arrays;
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    private Resume[] storage = new Resume[10000];
+    //private Resume[] storage = new Resume[10000];
+    private static Resume[] storage = new Resume[100];
+    static int storageLen, storageSize;
 
-//    public static void main(String[] args) {
-//        System.out.println(Arrays.toString(storage));
-//    }
+    public ArrayStorage() {
+        storageLen = storage.length;
+        storageSize = size();
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 20; i++) {
+            storage[i] = new Resume("resume " + i);
+        }
+        System.out.println(Arrays.toString(storage));
+        System.out.println(size());
+        System.out.println(checkEntry("resume 8"));
+        System.out.println(checkEntry("resume 888"));
+    }
 
     void clear() {
+        //неоптимально, надо через size() сделать
+        //можно сохрянять резервную копию массива на всякий (а надо ли???)
         Arrays.fill(storage, null);
     }
 
-    void save(Resume resume) {
-        //просто сохранить или проверить есть ли такой элеммент в массиве
+    static int save(Resume resume) {
+        if (checkEntry(resume.getUuid()) < 0) {
+            storage[size() + 1] = new Resume(resume.getUuid());
+        }
+        return checkEntry(resume.getUuid());
+        //можно хранить индекс первой свободной ячейки
         //создать массив который содержит индексы ненулевых элементов в рабочем массиве
-        //вместит ли отведенная память JVM массив из 10000 строк???
+        //по идее если делать ребейз, то в этом нет необходимости
     }
 
     Resume get(String uuid) {
+        //int arrayLen = size();
+        for (int i = 0; i < storageSize; i++) {
+
+        }
         return null;
     }
 
     void delete(String uuid) {
+        //если удаляется элемент в конце, то ребейз делать не надо
+        //и если удаляется элемент в начале (скажем в первой половине), то тоже ребейз не делаем.
+        //или можно хранить количество окон и если их становится больше какого-то числа, то сделать ребейз
     }
 
     /**
@@ -35,7 +61,28 @@ public class ArrayStorage {
         return new Resume[0];
     }
 
-    int size() {
-        return 0;
+    //можно этому методу передавать массив, но тогда нужно перегружать метод, так как работать
+    //с примитивами надо одним способом, а с объектами другим (?????)
+    static int size() {
+        int arraySize = 0;
+        //int arrayLen = storage.length;
+        for (int i = 0; i < storageLen; i++) {
+            if (storage[i] != null) {
+                arraySize += 1;
+            }
+        }
+        return arraySize;
+    }
+
+    private void normalize() {
+        //помещает все элемента мыссива в начало
+        //метод не так прост как кажется.
+    }
+
+    private static int checkEntry(String uuid) {
+        for (int i = 0; i < storageSize; i++) {
+            if (storage[i].getUuid() == uuid) return i;
+        }
+        return -1;
     }
 }
--- src/com/urise/webapp/model/Resume.java
+++ src/com/urise/webapp/model/Resume.java
@@ -6,7 +6,7 @@ public class Resume {
     private String uuid;// Unique identifier
 
     public Resume(String uuid) {
-        this.uuid = uuid;
+        this.uuid = uuid.intern();
     }
 
     public String getUuid() {
@@ -14,13 +14,18 @@ public class Resume {
     }
 
     public void setUuid(String uuid) {
-        this.uuid = uuid;
+        this.uuid = uuid.intern();
     }
+
+//    @Override
+//    public String toString() {
+//        return "Resume {" +
+//                "uuid = '" + uuid + '\'' +
+//                "}";
+//    }
 
     @Override
     public String toString() {
-        return "Resume {" +
-                "uuid = '" + uuid + '\'' +
-                " }";
+        return "uuid='" + uuid + '\'';
     }
 }
